ca6ba6f08c3b5fefb78c8fcd2eecb2832c029177
Kirk Buttermore <kbutterm@utas.edu.au>
2022-04-12 14:27:50 +1000

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whatever
+{
+    public enum Gender { M, F, X };
+    class Employee
+{
+    public string name { get; set; }
+    public int id;
+        public Gender gender;
+   
+
+    public override string ToString()
+    {
+        return "name: " + name + " id: " + id + " gender: " + gender;
+    }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whatever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<Employee> GenerateEmployees()
+                 foreach employee in employees
+               { Console.WriteLine(Employee)}
+        {
+            Employee e1= new Employee { name = "Jane", id = 1, gender = Gender.F };
+            Employee e2= new Employee { name = "Mary", id = 2, gender = Gender.F };
+            Employee e3 new Employee { name = "Scott", id = 3, gender = Gender.M };
+            Employee e4= new Employee { name = "Macbeth", id = 4, gender = Gender.M };
+            Employee e5= new Employee { name = "Jemima", id = 5, gender = Gender.F };
+            Employee e6= new Employee { name = "Fruity", id = 6, gender = Gender.F };
+
+            List<Employee> employees = new List<Employee>();
+
+            employees.Add(e1);
+            employees.Add(e2);
+            employees.Add(e3);
+            employees.Add(e4);
+            employees.Add(e5);
+            employees.Add(e6);
+ 
+            Employee e = new Employee()
+        }
+    }
+}
